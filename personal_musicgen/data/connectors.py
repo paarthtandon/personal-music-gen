fe10da16c58fecab78af093553ae03d0f11e9dd9
Paarth Tandon <paarthri@gmail.com>
2023-11-06 11:12:20 -0800

    download songs from youtube

--- personal_musicgen/data/connectors.py
+++ personal_musicgen/data/connectors.py
@@ -1,11 +1,14 @@
+import os
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import time
+import yt_dlp
 
 def get_saved_tracks_spotify(
         client_id: str,
         client_secret: str,
-        redirect_uri: str
+        redirect_uri: str,
+        limit: int = None
 ) -> list:
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id=client_id,
@@ -17,6 +20,8 @@ def get_saved_tracks_spotify(
     offset = 0
     songs = []
     while True:
+        if limit is not None and len(songs) >= limit:
+            break
         results = sp.current_user_saved_tracks(limit=50, offset=offset)
         if not results['items']:
             break
@@ -27,7 +32,34 @@ def get_saved_tracks_spotify(
             songs.append(f'{artist_name} - {song_name}')
 
         offset += 50
-        time.sleep(0.5)
 
     return songs
 
+def download_songs_from_youtube(
+        songs: list,
+        download_dir: str,
+        ffmpeg_loc: str
+) -> None:
+    if not os.path.isdir(download_dir):
+        os.makedirs(download_dir)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '32', 
+        }],
+        'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+        'default_search': 'ytsearch1:',
+        'quiet': True,
+        'no_warnings': True,
+        'ignoreerrors': True,
+        'ffmpeg_location': ffmpeg_loc
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        for track_string in songs:
+            print(f"Downloading: {track_string}")
+            ydl.download([track_string])
+            print(f"Finished downloading: {track_string}")
