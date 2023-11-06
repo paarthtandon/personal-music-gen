355ef19d158f63d38940b23b241aab755420193a
Paarth Tandon <paarthri@gmail.com>
2023-11-06 12:53:32 -0800

    download script

---
+++ download_data.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from personal_musicgen.data import connectors
+
+DATA_DIR = './data/wav/'
+
+load_dotenv()
+
+client_id = os.getenv('spotify_client_id')
+client_secret = os.getenv('spotify_client_secret')
+
+print(f'Collecting liked songs from Spotify...')
+
+track_strings = connectors.get_saved_tracks_spotify(
+    client_id,
+    client_secret,
+    'https://paarthtandon.com/'
+)
+
+print(f'Downloading {len(track_strings)} tracks from YouTube...')
+
+connectors.download_songs_from_youtube(
+    track_strings,
+    DATA_DIR,
+    ffmpeg_loc='./ffmpeg-6/bin/ffmpeg.exe'
+)
--- personal_musicgen/data/connectors.py
+++ personal_musicgen/data/connectors.py
@@ -3,6 +3,7 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import yt_dlp
+from tqdm import tqdm
 
 def get_saved_tracks_spotify(
         client_id: str,
@@ -36,7 +37,7 @@ def get_saved_tracks_spotify(
     return songs
 
 def download_songs_from_youtube(
-        songs: list,
+        track_strings: list,
         download_dir: str,
         ffmpeg_loc: str
 ) -> None:
@@ -48,18 +49,21 @@ def download_songs_from_youtube(
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '32', 
+            'preferredquality': '32'
+        }, {
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav'
         }],
         'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
         'default_search': 'ytsearch1:',
         'quiet': True,
-        'no_warnings': True,
-        'ignoreerrors': True,
+        'no_warnings': False,
+        'ignoreerrors': False,
         'ffmpeg_location': ffmpeg_loc
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        for track_string in songs:
-            print(f"Downloading: {track_string}")
+        for i, track_string in enumerate(track_strings):
             ydl.download([track_string])
-            print(f"Finished downloading: {track_string}")
+            if i % 10 == 0:
+                print(f'Downloaded {i + 1}/{len(track_strings)} songs')
