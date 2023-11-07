36f23d9aefb3667bb70b3a744365585d14dc6615
Paarth Tandon <paarthri@gmail.com>
2023-11-06 22:06:17 -0800

    added dataset

---
+++ personal_musicgen/data/datasets.py
@@ -0,0 +1,35 @@
+from torch.utils.data import Dataset
+import os
+
+class AudioDataset(Dataset):
+    def __init__(self, data_dir, no_label=False):
+        self.data_dir = data_dir
+        self.data_map = []
+
+        dir_map = os.listdir(data_dir)
+        for d in dir_map:
+            name, ext = os.path.splitext(d)
+            if ext == ".wav":
+                if no_label:
+                    self.data_map.append({"audio": os.path.join(data_dir, d)})
+                    continue
+                if os.path.exists(os.path.join(data_dir, name + ".txt")):
+                    self.data_map.append(
+                        {
+                            "audio": os.path.join(data_dir, d),
+                            "label": os.path.join(data_dir, name + ".txt"),
+                        }
+                    )
+                else:
+                    raise ValueError(f"No label file for {name}")
+
+    def __len__(self):
+        return len(self.data_map)
+
+    def __getitem__(self, idx):
+        data = self.data_map[idx]
+        audio = data["audio"]
+        label = data.get("label", "")
+
+        return audio, label
+    
